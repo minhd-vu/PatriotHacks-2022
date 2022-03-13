00c315f642aa43b3fbf562613049a8701e504106
Minh Vu <minhd_vu@yahoo.com>
2022-03-13 03:14:46 -0400

    update about

--- microsoft/src/components/about.js
+++ microsoft/src/components/about.js
@@ -4,11 +4,10 @@ export default function About() {
     return (
         <div>
             <h3>About</h3>
-            <h5>Help refugees connect with loved ones?</h5>
-            <h5>Help refugees notify loved ones that they are currently safe?</h5>
-            <h5>Help refugees share information about their situations?</h5>
-            <h5>Enable access financial support in order to make purchase?</h5>
-            <h5>Send donations / money to other loved ones?</h5>
+            <h5>Inspiration</h5>
+            <p>With the current refugee crisis and conflict in Ukraine, we were worried that people were losing contact with their loved ones. Our web application was inspired by both the Citizen App and Waze Maps in the ways that both applications allowed people to work together to share vital information quickly and effortlessly. We wanted our web app to allow users to connect with their loved ones, locate nearby shelters, disseminate information about dangerous situations, and facilitate financial services and assistance.</p>
+            <h5>Features</h5>
+            <p>Users can create a profile with their personal information (e.g. names, photographs, date of birth) to quickly connect and find other users and support services. Our web application allows users to check on the status of their loved ones, observe a heat map to circumvent dangerous areas, chat with other users, and have financial services with the use of a cryptocurrency wallet.</p>
         </div>
     );
 }
